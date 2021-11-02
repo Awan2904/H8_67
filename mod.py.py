8a746d8ec29e7eaa84c13ffa51d5f08c8be6a0e5
awankoding <indriawanawan@gmail.com>
2021-11-02 11:00:17 +0700

    Sesi 3

---
+++ mod.py.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+s = "Hacktiv8-PTP Python For Data Science"
+a = [100, 200, 300]
+
+def foo(arg): 
+    print(f'arg = {arg}')
+    
+class Foo: 
+    pass 
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
---
+++ pkg/mod1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def foo(): 
+    print('[mod] foo()')
+
+class Foo: 
+    pass
+
+
+# In[ ]:
+
+
+
+
